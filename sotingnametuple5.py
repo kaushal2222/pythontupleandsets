716e6da99638049f594c9f272f6f9c27a633c045
kaushal2222 <158256664+kaushal2222@users.noreply.github.com>
2024-03-27 11:13:32 +0530

    Create sotingnametuple5.py

---
+++ sotingnametuple5.py
@@ -0,0 +1,17 @@
+def read_list_items():
+ print("Enter names separated by a space")
+ list_items = input().split()
+ return list_items
+def sort_item_list(items_in_list):
+ n = len(items_in_list)
+ for i in range(n):
+   for j in range(1, n-i):
+      if items_in_list[j-1] > items_in_list[j]:
+         (items_in_list[j-1], items_in_list[j]) = (items_in_list[j], items_in_list[j-1])
+ print("After Sorting")
+ print(items_in_list)
+def main():
+ all_items = read_list_items()
+ sort_item_list(all_items)
+if __name__ == "__main__":
+ main()
