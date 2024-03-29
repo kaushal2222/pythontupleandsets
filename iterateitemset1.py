68db1f01f573927ec73a1348a4a1edb4fa4fdb37
kaushal2222 <158256664+kaushal2222@users.noreply.github.com>
2024-03-29 10:28:14 +0530

    Create iterateitemset1.py

---
+++ iterateitemset1.py
@@ -0,0 +1,6 @@
+warships = {"u.s.s._arizona", "hms_beagle", "ins_airavat", "ins_hetz"}
+def main():
+    for eachship in warships:
+        print(f"{eachship} is warships")
+if __name__ == "__main__":
+ main()
