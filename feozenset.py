9afda64febdc66d16c4bdfd77d4465d2fab6b1d2
kaushal2222 <158256664+kaushal2222@users.noreply.github.com>
2024-03-29 10:30:28 +0530

    Create feozenset.py

---
+++ feozenset.py
@@ -0,0 +1,12 @@
+fs = frozenset(["g", "o", "o", "d"])
+print(fs) 
+animals = set([fs, "cattle", "horse"])
+print(animals)
+#
+official_languages_world = {"english":59, "french":29, "spanish":21}
+print(frozenset(official_languages_world))
+#
+frs=frozenset(["germen"])
+official_languages_world = {"english":59, "french":29, "spanish":21, frs:6}
+print(official_languages_world)
+
